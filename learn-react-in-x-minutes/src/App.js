8dba9b5938e95344da754544e50ceeb2c5d2851a
achutya1968 <39107934+achutya1968@users.noreply.github.com>
2021-07-18 12:15:48 -0400

    app test

---
+++ learn-react-in-x-minutes/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  return ( 
+    < >
+    <TodoList />
+    <input type = "text" />
+    </>
+  )
+}
+
+export default App;
