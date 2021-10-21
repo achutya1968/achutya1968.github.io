b5a046c0b0c59e51e1dfe74c339405dea81bb238
achutya1968 <39107934+achutya1968@users.noreply.github.com>
2021-10-20 21:48:02 -0400

    Update App.js

--- learn-react-in-x-minutes/src/App.js
+++ learn-react-in-x-minutes/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React,{ useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([])
   return ( 
     < >
     <TodoList />
     <input type = "text" />
+    <button>Add Todo</button>
+    <button>Clear completed Todos</button>
+    <div>0 left to do</div>
     </>
   )
 }
